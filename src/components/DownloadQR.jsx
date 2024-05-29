5ea65e1f3eaa11fce7acb771f1782b3ce8fbe0ea
Adarsh Malviya <adarshmalviya1199@gmail.com>
2024-05-29 11:50:28 +0530

    qr code layout fixed

--- src/components/DownloadQR.jsx
+++ src/components/DownloadQR.jsx
@@ -1,6 +1,11 @@
 import QRCode from "qrcode.react";
+import { FaCopy } from "react-icons/fa";
 
 const DownloadQR = ({ imageLink }) => {
+  const copyToClipboard = () => {
+    navigator.clipboard.writeText(imageLink);
+  };
+
   const downloadQRImage = () => {
     const canvas = document.getElementById("qr-code");
     const pngUrl = canvas
@@ -15,15 +20,33 @@ const DownloadQR = ({ imageLink }) => {
   };
 
   return (
-    <div className=" mt-1 flex w-96 flex-col items-center justify-center">
-      <QRCode
-        id="qr-code"
-        value={imageLink}
-        size={290}
-        level={"H"}
-        includeMargin={true}
-      />
-      <a onClick={downloadQRImage} className="text-primary">
+    <div className=" mt-1 flex w-100 flex-col items-center justify-center">
+      <div className="border-3 mt-2 bg-white p-2 ">
+        <div className="border-3 border-stroke px-4.5 py-2 text-xs  ">
+          <p>
+            <span className="font-bold">Refer students </span>-
+          </p>
+
+          <p className="flex items-center justify-center gap-2">
+            <span className="text-base"> {imageLink} </span>
+            <FaCopy
+              onClick={copyToClipboard}
+              style={{ cursor: "pointer" }}
+              className="transform transition-transform active:scale-125"
+            />
+          </p>
+        </div>
+
+        <QRCode
+          id="qr-code"
+          value={imageLink}
+          size={290}
+          level={"H"}
+          includeMargin={true}
+        />
+      </div>
+
+      <a onClick={downloadQRImage} className="cursor-pointer text-primary">
         Download QR
       </a>
     </div>
--- src/components/partner/Home.jsx
+++ src/components/partner/Home.jsx
@@ -1,4 +1,3 @@
-import { FaCopy } from "react-icons/fa";
 import { useSelector } from "react-redux";
 import DownloadQR from "../DownloadQR";
 
@@ -7,30 +6,12 @@ const Home = () => {
 
   const referralLink = `www.${location.hostname}:${location.port}/refer/${userInfo?.uniqueUserId}`;
 
-  const copyToClipboard = () => {
-    navigator.clipboard.writeText(referralLink);
-  };
-
   return (
     <div className=" flex-col justify-center text-center">
       <h1 className="text-xl font-semibold text-black dark:text-white">
         Welcome! You Have logged in Successfully
       </h1>
-      <div className="mt-2 w-100 bg-white p-2 ">
-        <div className="border-3  flex items-center justify-between border-stroke px-4.5 py-2 text-xs  ">
-          <p>
-            <span className="font-bold">Refer students </span>-
-            <span className=""> {referralLink} </span>
-          </p>
-
-          <span
-            onClick={copyToClipboard}
-            style={{ cursor: "pointer" }}
-            className="transform transition-transform active:scale-125"
-          >
-            <FaCopy className="" />
-          </span>
-        </div>
+      <div className="mt-2 w-100 border bg-white p-2 ">
         <DownloadQR imageLink={referralLink} />
       </div>
     </div>
